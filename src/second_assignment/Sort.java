01eaffe40a142f7b9ec0e647cb33d95349aaa061
Parley Martins <parleypachecomartins@gmail.com>
2015-10-28 14:58:40 -0600

    javadoc

--- src/second_assignment/Sort.java
+++ src/second_assignment/Sort.java
@@ -23,12 +23,25 @@ public class Sort {
 
 	private final int originalList[];
 
-	public Sort(int list[]) {
+	/**
+	 * This constructs a class Sort. Has two types of sort implemented, quick
+	 * and merge.
+	 * 
+	 * @param list
+	 *            The original list to be sorted.
+	 */
+	public Sort(int[] list) {
 		this.originalList = list;
 		this.mergeList = list.clone();
 		this.quickList = list.clone();
 	}
 
+	/**
+	 * The main function. Runs the sorts and prints the results
+	 * 
+	 * @param args
+	 *            Standard parameter
+	 */
 	public static void main(String[] args) {
 		String FOLDER = "C:\\Users\\Parley\\Documents\\GitHub\\csc310\\src";
 		String INPUT_FILE = FOLDER + "\\second_assignment\\data.txt";
@@ -54,6 +67,10 @@ public class Sort {
 		}
 	}
 
+	/**
+	 * This sorts the list using merge sort. It resets all counters and
+	 * calculate the time the sort takes to run.
+	 */
 	public void mergeSort() {
 		mergeKeyComp = 0;
 		mergeKeyMoves = 0;
@@ -62,6 +79,10 @@ public class Sort {
 		mergeTime = System.nanoTime() - mergeTime;
 	}
 
+	/**
+	 * This sorts the list using quick sort. It resets all counters and
+	 * calculate the time the sort takes to run.
+	 */
 	public void quickSort() {
 		quickKeyComp = 0;
 		quickKeyMoves = 0;
@@ -70,28 +91,55 @@ public class Sort {
 		quickTime = System.nanoTime() - quickTime;
 	}
 
+	/**
+	 * This prints the key movements, key comparisons and execution time of
+	 * merge sort
+	 */
 	public void printMergeStatics() {
 		out.println("mergeSort: ");
 		printStatics(mergeKeyComp, mergeKeyMoves, mergeTime);
 	}
 
+	/**
+	 * This prints the key movements, key comparisons and execution time of
+	 * quick sort
+	 */
 	public void printQuickStatics() {
 		out.println("quickSort: ");
 		printStatics(quickKeyComp, quickKeyMoves, quickTime);
 	}
 
+	/**
+	 * This gets the list sorted by quick sort.
+	 * 
+	 * @return It returns an array of integers
+	 */
 	public int[] getQuickList() {
 		return quickList;
 	}
 
+	/**
+	 * This gets the list sorted by merge sort.
+	 * 
+	 * @return It returns an array of integers
+	 */
 	public int[] getMergeList() {
 		return mergeList;
 	}
 
+	/**
+	 * This gets the unsorted list.
+	 * 
+	 * @return It returns an array of integers
+	 */
 	public int[] getOriginalList() {
 		return originalList;
 	}
 
+	/**
+	 * This prints the original list, runs quick sort and prints its statistics
+	 * and the sorted list
+	 */
 	public void printQuickSortResults() {
 		out.print("Running QuickSort on list: ");
 		printList(getOriginalList());
@@ -101,6 +149,10 @@ public class Sort {
 		printQuickStatics();
 	}
 
+	/**
+	 * This prints the original list, runs merge sort and prints its statistics
+	 * and the sorted list
+	 */
 	public void printMergeSortResults() {
 		out.print("Running MergeSort on list: ");
 		printList(getOriginalList());
@@ -110,6 +162,13 @@ public class Sort {
 		printMergeStatics();
 	}
 
+	/**
+	 * This reads an input line and returns the correspondent array.
+	 * 
+	 * @param in
+	 *            The Scanner where the input is
+	 * @return an array of integers
+	 */
 	private static int[] getLine(Scanner in) {
 		Integer size = in.nextInt();
 		int[] numbers = new int[size];
@@ -119,6 +178,14 @@ public class Sort {
 		return numbers;
 	}
 
+	/**
+	 * The actual sort algorithm. It's a recursive algorithm that is first
+	 * called from the public interface with the original list.
+	 * 
+	 * @param list
+	 *            The unsorted list
+	 * @return a sorted array of integers
+	 */
 	private int[] mergeSort(int[] list) {
 		int size = list.length;
 		if (size <= 3)
@@ -127,6 +194,7 @@ public class Sort {
 			int halfSize = (size / 2);
 			int[] firstHalf = Arrays.copyOfRange(list, 0, halfSize);
 			int[] secondHalf = Arrays.copyOfRange(list, halfSize, size);
+
 			firstHalf = mergeSort(firstHalf);
 			secondHalf = mergeSort(secondHalf);
 			list = mergeLists(firstHalf, secondHalf, size);
@@ -134,6 +202,17 @@ public class Sort {
 		return list;
 	}
 
+	/**
+	 * This merges the lists after the merge sort runs.
+	 * 
+	 * @param firstHalf
+	 *            an array of integers with the first half of the entire list
+	 * @param secondHalf
+	 *            an array of integers with the second half of the entire list
+	 * @param size
+	 *            the size that the merged list has to have
+	 * @return Returns the an array of integers with two sorted lists merged
+	 */
 	private int[] mergeLists(int[] firstHalf, int[] secondHalf, int size) {
 		int mergedList[] = new int[size];
 		int firstIt = 0;
@@ -154,7 +233,7 @@ public class Sort {
 		int smallestIt = 0;
 		int[] unfinishedList;
 		int halfSize = 0;
-		if (firstIt > secondIt) {
+		if (firstIt >= secondIt) {
 			smallestIt = secondIt;
 			unfinishedList = secondHalf;
 			halfSize = secondHalf.length;
@@ -172,6 +251,22 @@ public class Sort {
 		return mergedList;
 	}
 
+	/**
+	 * The base case to comparisons. It's called when a list has size 3 or less.
+	 * It's also used in the median of three, and that's why one has to pass the
+	 * three positions where the sort must happen.
+	 * 
+	 * @param list
+	 *            The small list to be sorted.
+	 * @param pos1
+	 *            The first position to be sorted.
+	 * @param pos2
+	 *            The second position to be sorted.
+	 * @param pos3
+	 *            The third position to be sorted.
+	 * @return the same list that came as parameter with the positions passed
+	 *         sorted. If the size is 3 or less the list is completely sorted.
+	 */
 	private int[] baseCase(int[] list, int pos1, int pos2, int pos3) {
 		if (list.length > 1 && pos3 - pos1 >= 1) {
 			if (list[pos1] > list[pos2]) {
@@ -195,6 +290,18 @@ public class Sort {
 		return list;
 	}
 
+	/**
+	 * Swaps two positions in a given list.
+	 * 
+	 * @param list
+	 *            The list in which the swap must happen.
+	 * @param pos1
+	 *            The position of the first element to be swapped.
+	 * @param pos2
+	 *            The position of the second element to be swapped.
+	 * @return the given list with the two elements in the given positions
+	 *         swapped.
+	 */
 	private int[] swap(int[] list, int pos1, int pos2) {
 		int temp = list[pos1];
 		list[pos1] = list[pos2];
@@ -204,15 +311,24 @@ public class Sort {
 		return list;
 	}
 
-	private void quickSort(final int begin, final int end) {
-		if ((end - begin) < 3) {
-			baseCase(quickList, begin, begin + 1, end);
+	/**
+	 * The actual quick sort algorithm. It's recursive and the first call
+	 * happens on {@link Sort#quickSort()}
+	 * 
+	 * @param first
+	 *            the position of the first element of the list to be sorted.
+	 * @param last
+	 *            the position of the last element of the list to be sorted.
+	 */
+	private void quickSort(final int first, final int last) {
+		if ((last - first) < 3) {
+			baseCase(quickList, first, first + 1, last);
 		} else {
-			int pivotPosition = (begin + end) / 2;
-			baseCase(quickList, begin, pivotPosition, end);
-			int low = begin + 1;
-			int high = end - 1;
-			while (low <= high && low < end) {
+			int pivotPosition = (first + last) / 2;
+			baseCase(quickList, first, pivotPosition, last);
+			int low = first + 1;
+			int high = last - 1;
+			while (low <= high && low < last) {
 				if (low > pivotPosition) {
 					swap(quickList, low, pivotPosition);
 					low = pivotPosition;
@@ -223,22 +339,37 @@ public class Sort {
 					swap(quickList, low, high);
 					if (high == pivotPosition) {
 						pivotPosition = low;
-						// quickKeyMoves++;
 					}
 					high--;
 					quickKeyComp += 2;
 				}
 				low++;
 			}
-			quickSort(begin, pivotPosition);
-			quickSort(pivotPosition + 1, end);
+			quickSort(first, pivotPosition);
+			quickSort(pivotPosition + 1, last);
 		}
 	}
 
+	/**
+	 * A wrapper to print the whole list.
+	 * 
+	 * @param list
+	 *            list to be printed.
+	 */
 	private void printList(final int[] list) {
 		printList(list, 0, list.length);
 	}
 
+	/**
+	 * Prints the given list, from a given start point to a given end point
+	 * 
+	 * @param list
+	 *            the list to be printed
+	 * @param begin
+	 *            the first element to be printed.
+	 * @param end
+	 *            the last element to be printed.
+	 */
 	private void printList(final int[] list, int begin, int end) {
 		out.print("[ ");
 		for (int i = begin; i < end; i++) {
@@ -247,6 +378,16 @@ public class Sort {
 		out.println("] ");
 	}
 
+	/**
+	 * Prints the statistics of the sort algorithms.
+	 * 
+	 * @param keyComp
+	 *            The number of key comparisons made by an algorithm.
+	 * @param keyMoves
+	 *            The number of key movements made by an algorithm.
+	 * @param time
+	 *            The time an algorithm took to run.
+	 */
 	private void printStatics(int keyComp, int keyMoves, long time) {
 		out.println("\t key comparisons: " + keyComp);
 		out.println("\t key movements: " + keyMoves);
