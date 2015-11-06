6c8378b0be7e0b2ae561b5db54d174b9e0f05c78
Parley Martins <parleypachecomartins@gmail.com>
2015-11-06 16:10:18 -0700

    Basic attributes and structure

--- src/third_assignment/Graph2015.java
+++ src/third_assignment/Graph2015.java
@@ -1,27 +1,37 @@
 package third_assignment;
 
 public class Graph2015 {
-	public Graph2015(int numberOfNodes){
-		
+	Node[] nodes;
+
+	public Graph2015(int numberOfNodes) {
+		nodes = new Node[numberOfNodes];
+		for (int i = 0; i < numberOfNodes; i++) {
+			Node node = new Node();
+			nodes[i] = node;
+		}
 	}
 
-	public void addEdge(int node1, int node2){
-		
+	public void addEdge(int node1, int node2) {
+
 	}
-	
-	public void setLabel(int nodeId, String label){
-		
+
+	public void setLabel(int nodeId, String label) {
+
 	}
-	
-	public void biConnect(){
-		
+
+	public void biConnect() {
+
 	}
-	
-	public void biConnect(String labelRoot){
-		
+
+	public void biConnect(String labelRoot) {
+
 	}
-	
-	public void biConnect(int nodeRoot){
-		
+
+	public void biConnect(int nodeRoot) {
+
+	}
+
+	public Node[] getNodes() {
+		return nodes;
 	}
 }
--- src/third_assignment/Node.java
+++ src/third_assignment/Node.java
@@ -1,5 +1,38 @@
 package third_assignment;
 
-public class Node {
+import java.util.ArrayList;
 
+public class Node {
+	private String label;
+	private boolean articulationPoint;
+	private ArrayList<Integer> edges;
+
+	public Node() {
+		this(null);
+	}
+	
+	public Node(String label) {
+		this.label = label;
+		edges = new ArrayList<>();		
+	}
+	
+	public void addEdge(int node){
+		edges.add(node);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	
+	public boolean isArticulationPoint(){
+		return articulationPoint;
+	}
+	
+	public void setArticulationPoint(boolean ap){
+		articulationPoint = ap;
+	}
 }
