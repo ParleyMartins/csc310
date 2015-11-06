1df1828c4a0348389b7c1d0191be291c37ecece7
Parley Martins <parleypachecomartins@gmail.com>
2015-11-06 16:20:13 -0700

    Add colors

---
+++ src/third_assignment/Color.java
@@ -0,0 +1,7 @@
+package third_assignment;
+
+public enum Color {
+	WHITE,
+	GREY,
+	BLACK
+}
--- src/third_assignment/Node.java
+++ src/third_assignment/Node.java
@@ -6,17 +6,19 @@ public class Node {
 	private String label;
 	private boolean articulationPoint;
 	private ArrayList<Integer> edges;
+	public Color color;
 
 	public Node() {
 		this(null);
 	}
-	
+
 	public Node(String label) {
 		this.label = label;
-		edges = new ArrayList<>();		
+		edges = new ArrayList<>();
+		color = Color.WHITE;
 	}
-	
-	public void addEdge(int node){
+
+	public void addEdge(int node) {
 		edges.add(node);
 	}
 
@@ -27,12 +29,16 @@ public class Node {
 	public void setLabel(String label) {
 		this.label = label;
 	}
-	
-	public boolean isArticulationPoint(){
+
+	public boolean isArticulationPoint() {
 		return articulationPoint;
 	}
-	
-	public void setArticulationPoint(boolean ap){
+
+	public void setArticulationPoint(boolean ap) {
 		articulationPoint = ap;
+	}
+
+	public ArrayList<Integer> getEdges() {
+		return edges;
 	}
 }
