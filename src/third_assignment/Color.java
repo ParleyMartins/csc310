f31e5c2f87e0b51769d04cb3372255a7b5c1b833
Parley Martins <parleypachecomartins@gmail.com>
2015-11-23 15:40:37 -0700

    Documentation for other classes

--- src/third_assignment/Color.java
+++ src/third_assignment/Color.java
@@ -1,7 +1,12 @@
 package third_assignment;
 
+/**
+ * Enum for the colors
+ * 
+ * @author Parley Pacheco Martins 1484000 November 29th, 2015
+ *
+ */
+
 public enum Color {
-	WHITE,
-	GREY,
-	BLACK
+	WHITE, GREY, BLACK
 }
--- src/third_assignment/Node.java
+++ src/third_assignment/Node.java
@@ -2,6 +2,13 @@ package third_assignment;
 
 import java.util.ArrayList;
 
+/**
+ * Class to represent the nodes of a graph. It stores the label, if this node is
+ * an articulation point, the edges that come out of it and the order in which
+ * it was visited in the depth first search
+ * 
+ * @author Parley Pacheco Martins 1484000 November 29th, 2015
+ */
 public class Node {
 	private String label;
 	private boolean articulationPoint;
@@ -9,10 +16,19 @@ public class Node {
 	public Color color;
 	public int time;
 
+	/**
+	 * Constructs a node object without a given label
+	 */
 	public Node() {
 		this(null);
 	}
 
+	/**
+	 * Constructs a node object with a given label
+	 * 
+	 * @param label
+	 *            Label associated to this node
+	 */
 	public Node(String label) {
 		this.label = label;
 		edges = new ArrayList<>();
@@ -20,34 +36,75 @@ public class Node {
 		time = Integer.MAX_VALUE;
 	}
 
+	/**
+	 * Add an edge leaving this node
+	 * 
+	 * @param node
+	 *            the destination node
+	 */
 	public void addEdge(int node) {
 		edges.add(node);
 	}
 
+	/**
+	 * Gets the associated label
+	 * 
+	 * @return The label of this node
+	 */
 	public String getLabel() {
 		return label;
 	}
 
+	/**
+	 * Sets a new label to the node
+	 * 
+	 * @param label
+	 *            The new label
+	 */
 	public void setLabel(String label) {
 		this.label = label;
 	}
 
+	/**
+	 * Checks if the node is an articulation point
+	 * 
+	 * @return true if the node is an articulation point, false otherwise
+	 */
 	public boolean isArticulationPoint() {
 		return articulationPoint;
 	}
 
+	/**
+	 * Sets this node as an articulation point or not
+	 * 
+	 * @param ap
+	 *            true or false if this is an articulation point
+	 */
 	public void setArticulationPoint(boolean ap) {
 		articulationPoint = ap;
 	}
 
+	/**
+	 * Gets the list of edges
+	 * 
+	 * @return An ArrayList with all the edges
+	 */
 	public ArrayList<Integer> getEdges() {
 		return edges;
 	}
-	
-	public boolean isRoot(){
+
+	/**
+	 * Check is this node is a root in the depth first search
+	 * 
+	 * @return true if the node is root, false otherwise
+	 */
+	public boolean isRoot() {
 		return time == 1;
 	}
-	
+
+	/**
+	 * Returns the content of the node as a string
+	 */
 	@Override
 	public String toString() {
 		return getLabel();
